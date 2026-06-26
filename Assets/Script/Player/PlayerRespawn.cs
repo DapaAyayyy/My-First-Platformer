7906daa6bdbf7c687c02b1205e8469b5dc47e5ce
Dapa Ganteng <daffatiar721@gmail.com>
2026-06-27 04:59:38 +0700

    Menambahkan UI Game Over

--- Assets/Script/Player/PlayerRespawn.cs
+++ Assets/Script/Player/PlayerRespawn.cs
@@ -5,14 +5,27 @@ public class PlayerRespawn : MonoBehaviour
     [SerializeField] private AudioClip checkpointSound;
     private Transform currentCheckpoint;
     private Health playerHealth;
+    private UIManager uiManager;
 
     public void Awake()
     {
         playerHealth = GetComponent<Health>();
+        uiManager = FindObjectOfType<UIManager>();
     }
 
-    public void Respawn()
+    public void CheckRespawn()
     {
+
+        //Check apakah checkpoint masih tersedia(belum diambil)
+        if(currentCheckpoint == null)
+        {
+            //Menampilkan UI Game Over
+            uiManager.GameOver();
+
+            return; //disini berarti kita ga akan melakukan respawn
+        }
+
+
         transform.position = currentCheckpoint.position; //Moving player to the checkpoint when respawn
         playerHealth.Respawn(); //Restore Player health and reset animation
 
